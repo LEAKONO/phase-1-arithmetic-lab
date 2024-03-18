6e46a0289cdf8e8d61022670a0f5bd85ed77bcb8
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-03-19 00:10:21 +0300

    Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+    let num1 = 31;
+    let num2 = 2;
+    let multiply = num1 * num2;
+    
+    let random=5
+
+
+    let num3=18
+    let num4=7
+    let mod=18%7
+
+    let numbers="1,2,5,6,9,20"
+    let max=20
